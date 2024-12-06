96a4cf4aad4d6f4e6e3e97c98d5a3dc9383d5f55
AlanaSR <svetlanka.shkola@gmail.com>
2024-12-07 00:11:35 +0500

    initialization

---
+++ src/main/java/com/mokitohw/mokitohw/MokitohwApplication.java
@@ -0,0 +1,13 @@
+package com.mokitohw.mokitohw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokitohwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokitohwApplication.class, args);
+	}
+
+}
